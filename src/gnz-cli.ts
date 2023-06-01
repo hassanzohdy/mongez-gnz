cd74d4647fa0e5f3910dbf4076dbb99bbbe41d02
Hasan Zohdy <hassanzohdy@gmail.com>
2023-06-01 19:36:32 +0300

    "Fixing cli"

--- src/gnz-cli.ts
+++ src/gnz-cli.ts
@@ -1,21 +1,24 @@
 import { getJsonFile } from "@mongez/fs";
 import { program } from "commander";
-import { getGeneratorsList, welcome } from ".";
 import { generateUsingNodeApi } from "./generate-using-api";
+import { getGeneratorsList } from "./generators-list";
+import { welcome } from "./utils";
 
-welcome();
+export async function gnzCli() {
+  welcome();
 
-const packageJson = getJsonFile("package.json");
+  const packageJson = getJsonFile("package.json");
 
-program.version(packageJson.version);
-program.addCommand(generateUsingNodeApi);
+  program.version(packageJson.version);
+  program.addCommand(generateUsingNodeApi);
 
-for (const generator of getGeneratorsList()) {
-  if (!generator?.cliOptions?.commands) continue;
+  for (const generator of getGeneratorsList()) {
+    if (!generator?.cliOptions?.commands) continue;
 
-  for (const command of generator.cliOptions.commands) {
-    program.addCommand(command);
+    for (const command of generator.cliOptions.commands) {
+      program.addCommand(command);
+    }
   }
-}
 
-program.parse(process.argv);
+  program.parse(process.argv);
+}
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export {
   registerGenerator,
   registerGenerators,
 } from "./generators-list";
+export * from "./gnz-cli";
 export * from "./main";
 export * from "./types";
 export * from "./utils";
