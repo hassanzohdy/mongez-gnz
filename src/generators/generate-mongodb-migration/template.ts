dff1f4185dbe028987489164205df98c4d3dd459
hassanzohdy <hassanzohdy@gmail.com>
2024-06-18 15:21:33 +0300

    "Fixing blueprint import"

--- src/generators/generate-mongodb-migration/template.ts
+++ src/generators/generate-mongodb-migration/template.ts
@@ -1,4 +1,4 @@
-import { toCamelCase, toStudlyCase } from "@mongez/reinforcements";
+import { toStudlyCase } from "@mongez/reinforcements";
 import { formatDate } from "../../utils/format-date";
 import { format, newLine } from "./../../utils";
 import { MigrationTemplateOptions } from "./types";
@@ -17,17 +17,17 @@ export async function generateMigrationTemplate(
   const importPath = `../${modelFileName.replace(/^\.\//, "")}`;
 
   const content = `
-import { migrationOffice, type ModelBlueprint } from "@warlock.js/cascade";
+import { migrationOffice, type Blueprint } from "@warlock.js/cascade";
 import { ${modelClass} } from "${importPath}";
 
 export default migrationOffice.register({
   name: "${migrationFunctionName}",
   blueprint: ${toStudlyCase(modelClass)}.blueprint(),
   createdAt: "${formatDate()}",
-  up: (blueprint: ModelBlueprint) => {
+  up: (blueprint: Blueprint) => {
     ${indexes.join(newLine)}
   },
-  down: (blueprint: ModelBlueprint) => {
+  down: (blueprint: Blueprint) => {
     ${indexesDown.join(newLine)}
   },
 });
