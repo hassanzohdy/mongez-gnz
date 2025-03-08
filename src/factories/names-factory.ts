8894cf8789a151c057ba37be360a2d593b609fa6
hassanzohdy <hassanzohdy@gmail.com>
2025-03-08 14:21:05 +0200

    "Trimmed out reserved keyword in name factory"

--- src/factories/names-factory.ts
+++ src/factories/names-factory.ts
@@ -26,12 +26,15 @@ export const namesFactory = {
     return `/${toKebabCase(pluralize(name))}`;
   },
   repositoryClassName(name: string) {
+    name = name.replace(/Repository$/i, "");
     return `${pluralize(toStudlyCase(name))}Repository`;
   },
   repositoryExportName(name: string) {
+    name = name.replace(/Repository$/i, "");
     return `${pluralize(toCamelCase(name))}Repository`;
   },
   repositoryFilePath(name: string) {
+    name = name.replace(/Repository$/i, "");
     return `${toKebabCase(pluralize(name))}.repository`;
   },
   // Database Model
@@ -39,9 +42,11 @@ export const namesFactory = {
     return toKebabCase(pluralize(name, 1));
   },
   modelClassName(name: string) {
+    name = name.replace(/Model$/i, "");
     return toStudlyCase(pluralize(name, 1));
   },
   modelFilePath(name: string) {
+    name = name.replace(/Model$/i, "");
     return `${toKebabCase(pluralize(name, 1))}.model`;
   },
   modelFolderPath(name: string) {
@@ -49,9 +54,11 @@ export const namesFactory = {
   },
   // Output
   outputFilePath(name: string) {
+    name = name.replace(/Output$/i, "");
     return `${toKebabCase(pluralize(name, 1))}.output`;
   },
   outputClassName(name: string) {
+    name = name.replace(/Output$/i, "");
     return `${toStudlyCase(pluralize(name, 1))}Output`;
   },
   // Restful
@@ -65,9 +72,11 @@ export const namesFactory = {
     return `${toKebabCase(pluralize(name))}.restful`;
   },
   controllerName(name: string) {
+    name = name.replace(/Controller$/i, "");
     return `get${toStudlyCase(pluralize(name))}Controller`;
   },
   controllerFilePath(name: string) {
+    name = name.replace(/Controller$/i, "");
     return `${toKebabCase(pluralize(name))}.controller`;
   },
 };
